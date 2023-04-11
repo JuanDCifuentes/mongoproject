a6daf47a6a5320525b58d20ffe97f610162d2fc5
Juan David <juandcifuentes16@gmail.com>
2023-04-11 00:02:46 -0500

    v0.0.1

--- src/models/model-user.js
+++ src/models/model-user.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  name: {
+  Username: {
     type: String,
     required: true,
   },
-  age: {
-    type: Number,
-    required: true,
-  },
+
   email: {
     type: String,
     required: true,
   },
+
+  password: {
+    type: String,
+    require: true
+}
 });
 
 module.exports = mongoose.model("user", userSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -34,10 +34,10 @@ router.get('/users/:id', (req, res) => {
 // actualizar un usuario
 router.put('/users/:id', (req, res) => {
     const { id } = req.params;
-    const {name, age, email} = req.body;
+    const {Username, email, password} = req.body;
 
     userSchema
-        .updateOne({_id: id }, { $set:{ name, age, email} })
+        .updateOne({_id: id }, { $set:{ Username,email, password} })
         .then((data) => res.json(data))
         .catch((error) => res.json({messege: error}));
     
